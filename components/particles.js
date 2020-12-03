86daea2681c5029d5353010f48ab98ec8f253148
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-12-02 19:27:52 -0500

    Code cleanup

--- components/particles.js
+++ components/particles.js
@@ -1,18 +1,7 @@
-import { Geometry, DoubleSide, PointsMaterial, Quaternion, Spherical, SphereGeometry, TextureLoader, AdditiveBlending, Vector3, Points } from 'https://unpkg.com/three@0.117.0/build/three.module.js';
+import { PointsMaterial, SphereGeometry, TextureLoader, Vector3, Points } from 'https://unpkg.com/three@0.117.0/build/three.module.js';
   
 const Y_AXIS = new Vector3(0, 1, 0);
-const X_AXIS = new Vector3(1, 0, 0);
-const Z_AXIS = new Vector3(0, 0, 1);
 
-// var partGeo = new THREE.SphereGeometry(10, 64, 104);
-// var partGeo = new Geometry();
-var partGeo = new SphereGeometry(10.3, 3, 3, Math.PI / 6, Math.PI / 4, 0.25 * Math.PI, 0.104 * Math.PI);
-// var partGeo2 = new SphereGeometry(10.3, 3, 3, Math.PI / 6, Math.PI / 4, 0.25 * Math.PI, 0.104 * Math.PI);
-var partGeo2 = new SphereGeometry(10, 64, 30, 0, 2*Math.PI, 0, 0.25 * Math.PI);// detail of geom determines detail
-// var partGeos = [
-//     { year: "1913", geo: new SphereGeometry(10.3, 3, 3, Math.PI / 6, Math.PI / 4, 0.25 * Math.PI, 0.104 * Math.PI) },
-//     { year: "1990", geo: new SphereGeometry(10, 64, 30, 0, 2*Math.PI, 0, 0.25 * Math.PI) }//~
-// ];
 var partGeos = [
     new SphereGeometry(10.3, 3, 3, Math.PI / 6, Math.PI / 4, 0.25 * Math.PI, 0.104 * Math.PI),
     new SphereGeometry(10.3, 64, 30, 0, 2*Math.PI, 0, 0.25 * Math.PI)//~
@@ -26,13 +15,14 @@ var partMat = new PointsMaterial({
         map: new TextureLoader().load('../assets/textures/ladybug.png'), 
         transparent: true, 
         depthWrite: false
-        // blending: AdditiveBlending
 });
 
 // psystem.name = 'p_system';
 // scene.add(psystem);
 
 let pSystems = [];
+
+// add particle systems' geometries
 partGeos.forEach((geo, i) => {
     pSystems.push(new Points(geo, partMat));
 
@@ -44,21 +34,6 @@ partGeos.forEach((geo, i) => {
         pSystems[i].geometry.verticesNeedUpdate = true; 
     }
 });
-
-// pSystem.tick = _ => {
-//     pSystem.geometry.vertices.forEach(particle => {
-//         particle.applyAxisAngle(Y_AXIS, 0.001);
-//     });
-//     pSystem.geometry.verticesNeedUpdate = true; 
-// }; 
-
-// pSystem2.tick = _ => {
-//     pSystem2.geometry.vertices.forEach(particle => {
-//         particle.applyAxisAngle(Y_AXIS, 0.001);
-//     });
-    
-// 	pSystem2.geometry.verticesNeedUpdate = true; // can alter indiv. vertices in a plane, etc
-// }; 
 
 // let pSystems = [pSystem, pSystem2]
 export { pSystems };
